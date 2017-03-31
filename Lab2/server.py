a1d4965c64a1930a8a569bc0438ffd6060cec132
Austin Yates <austinyates400@gmail.com>
2017-03-30 23:57:13 -0400

    Server only accepts and parses valid commands

--- Lab2/server.py
+++ Lab2/server.py
@@ -1,12 +1,32 @@
 from socket import *
 serverPort = 12000
-serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('',serverPort))
-serverSocket.listen(1)
-print "The server is ready to receive."
-while 1:
-    connectionSocket, addr = serverSocket.accept()
-    sentence = connectionSocket.recv(1024)
-    capitalizedSentence = sentence.upper()
-    connectionSocket.send(capitalizedSentence)
-    connectionSocket.close()
+serverSocket = 0
+
+def main():
+    global serverSocket
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    serverSocket.bind(('',serverPort))
+    serverSocket.listen(1)
+    print "The server is ready to receive."
+    while 1:
+        connectionSocket, addr = serverSocket.accept()
+        clientInput = connectionSocket.recv(1024)
+        tokens = clientInput.split(' ', 1)
+        command = tokens[0]
+        if command == 'iWant':
+            connectionSocket.send('Request received for ' + tokens[1])
+            print 'Request received for ' + tokens[1]
+        elif command == 'uTake':
+            pass
+        else:
+            print ('That just ain\'t right! Bad command: '+ clientInput)
+            connectionSocket.send('That just ain\'t right!')
+        connectionSocket.close()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print 'Closing.'
+        serverSocket.shutdown(SHUT_RDWR)
+        serverSocket.close()
