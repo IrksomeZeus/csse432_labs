ea6966a937dfd2f97d2d1781007ad3771182487b
Austin Yates <austinyates400@gmail.com>
2017-03-30 23:05:11 -0400

    fixed another of my idiotic mistakes

--- Lab2/server.py
+++ Lab2/server.py
@@ -7,6 +7,6 @@ print "The server is ready to receive."
 while 1:
     connectionSocket, addr = serverSocket.accept()
     sentence = connectionSocket.recv(1024)
-    capitalizedSentence = sentence.ugpper()
+    capitalizedSentence = sentence.upper()
     connectionSocket.send(capitalizedSentence)
     connectionSocket.close()
