a95dba5e86ff617e65005646f20e3b45a5412591
Austin Yates <austinyates400@gmail.com>
2017-03-30 23:01:45 -0400

    fixed upper in server.py

--- Lab2/server.py
+++ Lab2/server.py
@@ -7,6 +7,6 @@ print "The server is ready to receive."
 while 1:
     connectionSocket, addr = serverSocket.accept()
     sentence = connectionSocket.recv(1024)
-    capitalizedMessage = sentence.uppper()
+    capitalizedMessage = sentence.upper()
     connectionSocket.send(capitalizedSentence)
     connectionSocket.close()
