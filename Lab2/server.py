40d32daf0d3628c60cd310f6f48c18c4a88917ca
Austin Yates <austinyates400@gmail.com>
2017-03-30 23:03:41 -0400

    fixed server.py variable name error{

--- Lab2/server.py
+++ Lab2/server.py
@@ -7,6 +7,6 @@ print "The server is ready to receive."
 while 1:
     connectionSocket, addr = serverSocket.accept()
     sentence = connectionSocket.recv(1024)
-    capitalizedMessage = sentence.upper()
+    capitalizedSentence = sentence.ugpper()
     connectionSocket.send(capitalizedSentence)
     connectionSocket.close()
